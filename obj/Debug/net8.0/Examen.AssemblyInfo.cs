af8695a28aca3f804fb5243090546dcf480b6304
Rafael-155 <ernesto_vera@usmp.pe>
2024-04-27 11:52:00 -0500

    fin

--- obj/Debug/net8.0/Examen.AssemblyInfo.cs
+++ obj/Debug/net8.0/Examen.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96832498d04a24ca425f6b9f310b6fa9547461dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bebe17ba865faf434aebe420f807dd4f1d166a84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Examen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
